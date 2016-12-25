029ace3671a51eff0de4f897bc027956b3b72e79
Yosuke Ishikawa <y@ishkawa.org>
2016-12-26 01:49:00 +0900

    Remove unused ResponseError cases

--- Sources/APIKit/Error/ResponseError.swift
+++ Sources/APIKit/Error/ResponseError.swift
@@ -5,11 +5,4 @@ import Foundation
 public enum ResponseError: Error {
     /// Indicates the session adapter returned `URLResponse` that fails to down-cast to `HTTPURLResponse`.
     case nonHTTPURLResponse(URLResponse?)
-
-    /// Indicates `HTTPURLResponse.statusCode` is not acceptable.
-    /// In most cases, *acceptable* means the value is in `200..<300`.
-    case unacceptableStatusCode(Int)
-
-    /// Indicates `Any` that represents the response is unexpected.
-    case unexpectedObject(Any)
 }
